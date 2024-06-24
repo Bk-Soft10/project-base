fee938eef5ce2d81e7e1e53de3cc698e140339bb
abubakr <bksoft10@gmail.com>
2024-06-24 11:42:01 +0300

    up

--- hide_odoo/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import mail_render_mixin
-from . import mail_mail
-from . import base
-from . import ir_module_module
-from . import res_config_settings
-from . import res_users
-from . import mail_channel
-# from . import ir_config_parameter
