7b92438d3b249f347b4eb8fd228e814539aacdae
odoo-dev <bksoft10@gmail.com>
2025-03-12 19:51:55 +0300

    up

--- sttl_sale_subscription/models/close_reason.py
+++ sttl_sale_subscription/models/close_reason.py
@@ -1,5 +1,5 @@
-from odoo import models, fields
 
+from odoo import models,fields 
 
 class CloseReason(models.Model):
     _name = 'close.reason'
@@ -7,3 +7,5 @@ class CloseReason(models.Model):
 
     name = fields.Char(string='name')
     desc = fields.Char(string='Description')
+    
+    
--- sttl_sale_subscription/models/product.py
+++ sttl_sale_subscription/models/product.py
@@ -1,4 +1,5 @@
-from odoo import fields, models, api
+from odoo import fields,models,api
+
 
 
 class Product(models.Model):
@@ -15,7 +16,7 @@ class Product(models.Model):
     #             if rec.product_tmpl_id:
     #                 for price in rec.product_tmpl_id.subscription_price_ids:
     #                     price.unlink()
-
+                    
     #                 for price in rec.subscription_price_ids:
     #                     rec.product_tmpl_id.subscription_price_ids.create({
     #                         "period_id": price.period_id.id,
@@ -23,7 +24,7 @@ class Product(models.Model):
     #                         "product_id": price.product_id.product_tmpl_id.id
     #                     })
     #                 rec.product_tmpl_id.is_recurring = True
-
+                
     #     return result
 
     # def write(self,vals):
@@ -34,7 +35,7 @@ class Product(models.Model):
     #                 if rec.product_tmpl_id:
     #                     for price in rec.product_tmpl_id.subscription_price_ids:
     #                         price.unlink()
-
+                        
     #                     for price in rec.subscription_price_ids:
     #                         rec.product_tmpl_id.subscription_price_ids.create({
     #                             "period_id": price.period_id.id,
@@ -53,23 +54,22 @@ class Product(models.Model):
     #                 price.unlink()
     #     return result
 
-
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     is_recurring = fields.Boolean("Recurring")
-    subscription_price_ids = fields.One2many(comodel_name='product.subscription.pricing', inverse_name='product_id')
+    subscription_price_ids = fields.One2many(comodel_name='product.subscription.pricing', inverse_name='product_id' )
 
     @api.model_create_multi
-    def create(self, vals):
-        result = super(ProductTemplate, self).create(vals)
+    def create(self,vals):
+        result = super(ProductTemplate,self).create(vals)
         for rec in result:
             if rec.is_recurring and len(rec.subscription_price_ids):
                 if len(rec.subscription_price_ids):
                     for variant in rec.product_variant_ids:
                         for price in variant.subscription_price_ids:
                             price.unlink()
-
+                    
                     for price in rec.subscription_price_ids:
                         for variant in rec.product_variant_ids:
                             variant.subscription_price_ids.create({
@@ -78,19 +78,19 @@ class ProductTemplate(models.Model):
                                 "product_id": variant.id
                             })
                             variant.is_recurring = True
-
+                
         return result
 
-    def write(self, vals):
-        result = super(ProductTemplate, self).write(vals)
+    def write(self,vals):
+        result = super(ProductTemplate,self).write(vals)
         for rec in self:
-            if rec.is_recurring:
+            if rec.is_recurring :
                 if len(rec.subscription_price_ids):
                     if len(rec.product_variant_ids):
                         for variant in rec.product_variant_ids:
                             for price in variant.subscription_price_ids:
                                 price.unlink()
-
+                            
                             for price in rec.subscription_price_ids:
                                 for variant in rec.product_variant_ids:
                                     variant.subscription_price_ids.create({
@@ -112,7 +112,6 @@ class ProductTemplate(models.Model):
                         price.unlink()
         return result
 
-
 class ProductSubscriptionPricing(models.Model):
     _name = 'product.subscription.pricing'
     _description = 'product subscription pricing'
@@ -122,6 +121,7 @@ class ProductSubscriptionPricing(models.Model):
     period_id = fields.Many2one(comodel_name='product.subscription.period', string='Period')
     product_id = fields.Many2one(comodel_name='product.product', string='Product')
 
+    
 
 class ProductSubscriptionPeriod(models.Model):
     _name = "product.subscription.period"
@@ -129,7 +129,7 @@ class ProductSubscriptionPeriod(models.Model):
 
     name = fields.Char(string='Name')
     duration = fields.Integer(string='Duration')
-    unit = fields.Selection(string='Unit',
-                            selection=[('days', 'Days'), ('weeks', 'Weeks'), ('month', 'Months'), ('year', 'Years')])
-
+    unit = fields.Selection(string='Unit', selection=[('days', 'Days'), ('weeks', 'Weeks'),('month','Months'),('year','Years')])
+    
     price_ids = fields.One2many(comodel_name='product.subscription.pricing', inverse_name='period_id', string='Pricing')
+        
