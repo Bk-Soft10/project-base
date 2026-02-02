69c97cf6c382b20a112732309ad0df41763c7974
am.ahmed <am.ahmed@takamol.com.sa>
2026-02-02 17:00:05 +0300

    update

--- purchase_from_sale/models/models.py
+++ purchase_from_sale/models/models.py
@@ -11,7 +11,8 @@ _logger = logging.getLogger(__name__)
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    request_purchase_ids = fields.One2many('purchase.order', 'request_sale_id', string='Purchases', copy=False)
+    request_purchase_ids = fields.One2many('purchase.order', 'request_sale_id', string='Purchases', copy=False,
+                                           domain=[('state', 'not in', ['cancel'])])
     request_purchase_count = fields.Integer("Count of RFQs", compute='_compute_request_purchase_count')
     can_confirm_so = fields.Integer("Can Confirm Order", compute='_compute_can_confirm_sale')
     state = fields.Selection(selection_add=[
@@ -75,6 +76,17 @@ class SaleOrder(models.Model):
         rec_su = self.sudo()
         rec_su.state = 'ready'
 
+    def action_cancel(self):
+        res = super().action_cancel()
+        for rec in self:
+            rec_su = rec.sudo()
+            po_recs = rec_su.request_purchase_ids.filtered(lambda x: x.state not in ['cancel'])
+            if any(order.locked or order.state in ['purchase', 'done'] for order in po_recs):
+                raise UserError(_("You cannot cancel a locked or confirmed order. Please unlock or confirm it first."))
+            for po_rec in po_recs:
+                po_rec.button_cancel()
+        return res
+
 #################################################################################################################
 # sale.order.line model
 #################################################################################################################
@@ -82,7 +94,12 @@ class SaleOrder(models.Model):
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    request_purchase_line_ids = fields.One2many('purchase.order.line', 'request_sale_line_id', string='Purchase Lines', copy=False)
+    request_purchase_line_ids = fields.One2many('purchase.order.line', 'request_sale_line_id', string='Purchase Lines',
+                                                copy=False, domain=[('order_id.state', 'not in', ['cancel'])])
+    price_vendor = fields.Float(string="Vendor Price", compute='_compute_vendor_price', digits='Product Price',
+                                store=True, readonly=True)
+    price_sale = fields.Float(string="Sale Price", compute='_compute_price_sale_unit', digits='Product Price',
+                              store=True, readonly=True)
 
     def can_create_request_po(self):
         self.ensure_one()
@@ -95,6 +112,41 @@ class SaleOrderLine(models.Model):
         purchase_lines = rec_su.request_purchase_line_ids.filtered(lambda x: x.order_id.state not in ['cancel'])
         return purchase_lines and len(purchase_lines) > 0 or False
 
+    @api.depends('product_id', 'product_uom_id', 'product_uom_qty')
+    def _compute_price_unit(self):
+        super()._compute_price_unit()
+        for rec in self:
+            rec_su = rec.sudo()
+            po_lines = rec.request_purchase_line_ids.filtered(lambda x: x.order_id.state not in ['cancel'])
+            if po_lines:
+                rec_su._compute_vendor_price()
+
+    @api.depends('request_purchase_line_ids', 'request_purchase_line_ids.price_unit', 'request_purchase_line_ids.order_id.state')
+    def _compute_vendor_price(self):
+        for rec in self:
+            rec_su = rec.sudo()
+            po_lines = rec_su.request_purchase_line_ids.filtered(lambda x: x.order_id.state not in ['cancel'])
+            rec_su.price_vendor = po_lines and po_lines[0].price_unit or 0.0
+            if rec_su.price_vendor > 0.0:
+                rec_su._compute_price_sale_unit()
+
+    @api.depends('product_id', 'product_uom_id', 'product_uom_qty', 'price_vendor', 'price_unit')
+    def _compute_price_sale_unit(self):
+        for rec in self:
+            rec_su = rec.sudo()
+            sale_rec = rec_su.order_id or None
+            company_rec = sale_rec and sale_rec.company_id or rec_su.company_id or self.env.company
+            price_type = company_rec and company_rec.sale_price_type or 'percentage'
+            sale_margin = company_rec and company_rec.sale_profit_margin or 5.0
+            vendor_price = rec_su.price_vendor or 0.0
+            profit_margin = rec_su.price_unit or 0.0
+            if vendor_price > 0.0:
+                if price_type == 'percentage':
+                    sale_margin = vendor_price * sale_margin / 100.0
+                profit_margin = sale_margin + vendor_price
+            rec_su.price_sale = profit_margin
+            rec.price_unit = profit_margin
+
 #################################################################################################################
 # purchase.order model
 #################################################################################################################
@@ -103,7 +155,7 @@ class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     partner_id = fields.Many2one(required=False)
-    request_sale_id = fields.Many2one('sale.order', string='Request Sale', copy=False)
+    request_sale_id = fields.Many2one('sale.order', string='Request Sale', copy=False, domain=[('state', 'not in', ['cancel'])])
     state = fields.Selection(selection_add=[
         ('sent',), ('vendor_price_confirmed', 'Vendor Price Confirmed'), ('to approve',),
     ], ondelete={'vendor_price_confirmed': 'set default'})
@@ -120,4 +172,5 @@ class PurchaseOrder(models.Model):
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    request_sale_line_id = fields.Many2one('sale.order.line', string='Request Sale Line', copy=False)
+    request_sale_line_id = fields.Many2one('sale.order.line', string='Request Sale Line', copy=False,
+                                           domain=[('order_id.state', 'not in', ['cancel'])])
